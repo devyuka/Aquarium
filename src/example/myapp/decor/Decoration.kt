440bd05ce8e0f7c59de6b373769ef80b65bdd95d
Yuka <yuka.0531.a@gmail.com>
2019-12-09 10:08:21 -0800

    Complete step10

--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -5,6 +5,14 @@ data class Decoration(val rocks: String){
 
 data class Decoration2(val rocks: String, val wood: String, val driver:String)
 
+enum class Color(val rgb: Int){
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF)
+}
+
+enum class Direction(val degrees: Int){
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 fun makeDecorations(){
     val decoration1 = Decoration("grantie")
     println(decoration1)
@@ -29,4 +37,7 @@ fun makeDecorations(){
 
 fun main() {
     makeDecorations()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
